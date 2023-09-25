e934a9c8ee7621f2e26ffc6a52396e2f29c8f223
Jung Beomjun <>
2023-09-25 19:39:34 +0900

    정범준 작업 버튼 디자인, 1번 통계자료 상세페이지 작성

--- parking/urls.py
+++ parking/urls.py
@@ -6,9 +6,13 @@ urlpatterns = [
     path("statistic_list/", views.statistic),
     path("seoul_map/",views.seoul_map),
     path("contents/", views.contents),
+    path("parking_count_detail_list/", views.parking_count_detail_list),
     path("parking_count_map/", views.parking_count_map),
+    path("seoul_circle/", views.seoul_circle),
     path("parking_expose_corr/", views.parking_expose_corr),
+    path("parking_expose_corr_detail_list/",views.parking_expose_corr_detail_list),
     path("transportation/", views.transportation),
     path("weather_corr/", views.weather_corr),
-    path("reference/",views.reference)
+    path("reference/",views.reference),
+
 ]
