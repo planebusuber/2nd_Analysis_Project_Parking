c2a539acf375385d15b63fc01b2b60344663d776
Jung Beomjun <>
2023-09-25 15:47:06 +0900

    웹 구성 작업 시

---
+++ parking/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("", views.landing),
+    path("statistic_list/", views.statistic),
+    path("seoul_map/",views.seoul_map),
+    path("contents/", views.contents),
+    path("parking_count_map/", views.parking_count_map),
+    path("parking_expose_corr/", views.parking_expose_corr),
+    path("transportation/", views.transportation),
+    path("weather_corr/", views.weather_corr),
+    path("reference/",views.reference)
+]
--- parking/views.py
+++ parking/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+
+
+def landing(request):
+    return render(request, template_name = "parking/single_page.html")
+
+def statistic(request):
+    return render(request, template_name = "parking/statistic_list.html")
+
+def seoul_map(request):
+    return render(request, template_name = "parking/seoul_map.html")
+
+def contents(request):
+    return render(request, template_name = "parking/contents.html")
+
+def parking_count_map(request):
+    return render(request, template_name = "parking/parking_count_map.html")
+
+def parking_expose_corr(request):
+    return render(request, template_name = "parking/parking_expose_corr.html")
+
+def transportation(request):
+    return render(request, template_name = "parking/transportation.html")
+
+def weather_corr(request):
+    return render(request, template_name = "parking/weather_corr.html")
+
+def reference(request):
+    return render(request, template_name = "parking/reference.html")
